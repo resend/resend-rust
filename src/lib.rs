529026467a7b42688f53394b86eabd4d81001c71
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-07-10 03:50:22 +0200

    Moved docs to README

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,4 @@
-//! A *very* minimal [Resend](https://resend.com) client for sending emails.
-//!
-//! Emails are sent via the [`resend_client::ResendClient`] which provides both a synchronous and asynchronous
-//! send method. The two are mutually exclusive and accessible via the `async` feature. The crate
-//! uses [`reqwest`](https://github.com/seanmonstar/reqwest) internally.
-//!
-//! Currently this only supports the `html` Resend parameter as I built this for my own use and
-//! that's all I need. If anyone else is looking into this however, I would not mind expanding it.
+#![doc = include_str!("../README.md")]
 
 pub mod error;
 pub mod mail;
